3a8ad1fef5eeb91d199a76412e7dff3dda1cdc23
DrKillex <lcollaro1994@gmail.com>
2024-11-18 15:42:32 +0100

    creazione tabella

--- demo/src/main/java/com/example/demo/note/Note.java
+++ demo/src/main/java/com/example/demo/note/Note.java
@@ -2,7 +2,18 @@ package com.example.demo.note;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "note_table")
 public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     public long getId() {
         return id;
